3fb83c92bf6709123125b92e0db77187c1bd7a57
druv-1122 <dhruvthakurella@gmail.com>
2026-06-07 17:15:14 +0530

    Delete Assignmen/Week1/Day2/Ques3.py

--- Assignmen/Week1/Day2/Ques3.py
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-        int l = 0;
-        int r = 0;
-        int n = nums.length;
-        double maxAvg = Integer.MIN_VALUE;
-
-        int sum = 0;
