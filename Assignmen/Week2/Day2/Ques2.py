c74114aed0191c045bb8997e2b2fb0a8cd072a86
druv-1122 <dhruvthakurella@gmail.com>
2026-06-10 01:29:17 +0530

    Implement checkInclusion method in Solution class

---
+++ Assignmen/Week2/Day2/Ques2.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        if len(s1) > len(s2):
+            return False
+
+        count1 = {}
+        count2 = {}
+
+        for ch in s1:
+            count1[ch] = count1.get(ch, 0) + 1
+
+        for i in range(len(s1)):
+            ch = s2[i]
+            count2[ch] = count2.get(ch, 0) + 1
+
+        if count1 == count2:
+            return True
+
+        left = 0
+
+        for right in range(len(s1), len(s2)):
+            count2[s2[right]] = count2.get(s2[right], 0) + 1
+
+            count2[s2[left]] -= 1
+            if count2[s2[left]] == 0:
+                del count2[s2[left]]
+
+            left += 1
+
+            if count1 == count2:
+                return True
+
+        return False
