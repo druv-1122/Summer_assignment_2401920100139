f611aa2411968629c79d13415d8a68e5db37434d
druv-1122 <dhruvthakurella@gmail.com>
2026-06-11 01:05:02 +0530

    Implement isSubsequence method in Solution class

---
+++ Assignmen/Week2/Day3/Ques2.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        if len(s) == 0:
+            return True
+
+        s_index = 0
+        t_index = 0
+
+        while t_index < len(t):
+            if s[s_index] == t[t_index]:
+                s_index += 1
+
+                if s_index == len(s):
+                    return True
+
+            t_index += 1
+
+        return False
